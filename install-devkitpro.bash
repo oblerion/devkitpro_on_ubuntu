80ea0d4d8c9d12e26bccc29e64636e430ebe8f49
oblerion <82583559+oblerion@users.noreply.github.com>
2021-04-22 00:55:05 +0200

    Update install-devkitpro.bash

--- install-devkitpro.bash
+++ install-devkitpro.bash
@@ -39,49 +39,49 @@ done
 if [ $c2 -gt 0 ] && [ $c2 -lt 9 ];then echo ">> just press enter for install";fi
 if [ $c2 -eq 1 ];then
     sudo dkp-pacman -S gba-dev
-    echo for exemples look at /opt/devkitpro/examples/gba
+    echo for examples look at /opt/devkitpro/examples/gba
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
 if [ $c2 -eq 2 ];then
     sudo dkp-pacman -S nds-dev
-    echo for exemples look at /opt/devkitpro/examples/nds
+    echo for examples look at /opt/devkitpro/examples/nds
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM    
 fi
 if [ $c2 -eq 3 ];then
     sudo dkp-pacman -S 3ds-dev
-    echo for exemples look at /opt/devkitpro/examples/3ds
+    echo for examples look at /opt/devkitpro/examples/3ds
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
 if [ $c2 -eq 4 ];then
     sudo dkp-pacman -S gamecube-dev
-    echo for exemples look at /opt/devkitpro/examples/gamecube
+    echo for examples look at /opt/devkitpro/examples/gamecube
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
 if [ $c2 -eq 5 ];then
     sudo dkp-pacman -S wii-dev
-    echo for exemples look at /opt/devkitpro/examples/wii
+    echo for examples look at /opt/devkitpro/examples/wii
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
 if [ $c2 -eq 6 ];then
     sudo dkp-pacman -S wiiu-dev
-    echo for exemples look at /opt/devkitpro/examples/wiiu
+    echo for examples look at /opt/devkitpro/examples/wiiu
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
 if [ $c2 -eq 7 ];then
     sudo dkp-pacman -S switch-dev
-    echo for exemples look at /opt/devkitpro/examples/switch
+    echo for examples look at /opt/devkitpro/examples/switch
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
 if [ $c2 -eq 8 ];then
     sudo dkp-pacman -S gp32-dev
-    echo for exemples look at /opt/devkitpro/examples/gp32
+    echo for examples look at /opt/devkitpro/examples/gp32
     export DEVKITPRO=/opt/devkitpro/
     export DEVKITARM=/opt/devkitpro/devkitARM
 fi
