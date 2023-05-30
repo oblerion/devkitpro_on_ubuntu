6e1fcc59dfaa47d9617227b3b13d3cdfe05cc322
oblerion <82583559+oblerion@users.noreply.github.com>
2023-05-30 18:32:15 +0200

    Update remove-devkitpro.bash

--- remove-devkitpro.bash
+++ remove-devkitpro.bash
@@ -1,10 +1,12 @@
 #! /bin/bash 
-# version 0.1
+# version 0.2
 if ! [ "$(id -u)" = 0 ]; then
   echo "Need root privilege to remove!"
   exit 1
 fi
+
 sudo apt-get remove devkitpro-pacman
+sudo apt-get purge devkitpro-pacman
 sudo rm -r /opt/devkitpro
 sudo rm -r /etc/apt/sources.list.d/devkitpro.list
 #sudo rm /etc/profile.d/devkit-env.sh
